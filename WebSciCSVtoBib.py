c9be6d94e625aeafe9ceb33ac036f43f8bd50ec0
Tiago Heineck <Tiago Heineck>
2016-02-23 19:16:40 -0300

    ajustes

--- WebSciCSVtoBib.py
+++ WebSciCSVtoBib.py
@@ -10,7 +10,7 @@ with open(file,'rb') as csvfile:
 	spamreader = csv.reader(csvfile,delimiter='\t')		
 	i = 1;
 	for row in spamreader:				
-		texto += '@Article{WEBOFSCI' + str(i) + ',\n' + '\t' + 'author={' + row[1] + '},\n' +  '\t' + 'title={' + row[9] + '},\n' + '\t' + 'journal={' + row[17] + '},\n' +'\t' + 'year={' + row[32] + '},\n' +		'\t' + 'abstract={' + row[33] + '}\n' +		'}\n\n'
+		texto += '@article{Webofsci' + str(i) + ',\n' + '\t' + 'author = {' + row[1] + '},\n' +  '\t' + 'title = {' + row[9] + '},\n' + '\t' + 'journal = {' + row[17] + '},\n' +'\t' + 'year = {' + row[32] + '},\n' +		'\t' + 'abstract = {' + row[33] + '},\n'  +		'}\n\n'
 		i += 1
 with open(bibFile,'a') as arq:
 	arq.write(texto)	
